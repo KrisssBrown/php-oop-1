b1a9c4f54ef22c21461a15d5c32ab780946db527
KrisssBrown <cristianbruno.cs@gmail.com>
2023-04-02 13:46:48 +0200

    start

---
+++ models/movies.php
@@ -0,0 +1,16 @@
+<?php
+
+
+//Creao la classe movie
+class Movie{
+
+    //Definisco delle variabili d'istanza nella classe movie e ne specifico il tipo
+    public string $title;
+    public string $director;
+    public string $genre;
+    public string $description;
+    public string $cast;
+    public float $rating;
+    public float $duration;
+
+}
